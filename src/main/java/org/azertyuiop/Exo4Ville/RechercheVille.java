7176be92485cc75a4d57d46ae39a17b64e8c204b
palikcuiho <>
2024-10-17 11:21:57 +0200

    Tests passed ✅

--- src/main/java/org/azertyuiop/Exo4Ville/RechercheVille.java
+++ src/main/java/org/azertyuiop/Exo4Ville/RechercheVille.java
@@ -3,14 +3,31 @@ package org.azertyuiop.Exo4Ville;
 import jdk.jshell.spi.ExecutionControl;
 import org.azertyuiop.exception.ArgumentTooShortException;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 public class RechercheVille {
-   private List<String> villes;
+   List<String> villes;
 
-   public List<String> rechercher(String mot) throws ArgumentTooShortException {
-     if (mot.length() < 2)
-      throw new ArgumentTooShortException("Le texte de la recherche doit contenir plus de deux caractères");
-     return List.of();
+  public RechercheVille(List<String> villes) {
+    this.villes = villes;
+  }
+
+  public List<String> rechercher(String mot) throws ArgumentTooShortException {
+
+     if (mot.length() < 2) {
+       if (Objects.equals(mot, "*"))
+         return villes;
+       throw new ArgumentTooShortException("Le texte de la recherche doit contenir plus de deux caractères");
+     }
+     else {
+       List<String> result = new ArrayList<>();
+       villes.forEach(v -> {
+         if (v.toLowerCase().contains(mot.toLowerCase()))
+           result.add(v);
+       });
+       return result;
+     }
    }
 }
